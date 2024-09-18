5822499f7faaef5466d6a5a3441a5d667cdd097b
amanrawt03 <amanrawat3021@gmail.com>
2024-09-18 13:16:34 +0530

    5. Load more updated

--- src/components/Load More/Loading.jsx
+++ src/components/Load More/Loading.jsx
@@ -1,32 +1,51 @@
 import React, { useEffect, useState } from "react";
-import { DiVim } from "react-icons/di";
+import "./style.css";
 
-const Loading = ({ url }) => {
+const Loading = () => {
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [count, setCount] = useState(0);
   async function fetchURL() {
     try {
-      const res = await fetch(url);
-      const data = res.json();
+      const res = await fetch(
+        `https://dummyjson.com/products?limit=20&skip=${
+          count === 0 ? 0 : count * 20
+        }`
+      );
+      const data = await res.json();
       if (data && data.products) {
-        setItems(data.products);
-        setLoading(false)
+        setItems([...items, ...data.products]);
+        setLoading(false);
       }
     } catch (error) {
       console.log(error);
     }
   }
-  useEffect(()=>{
-    if(url!== "")fetchURL();
-  }, [url])
-  if(loading){
-    return <div>Loading your data . Please wait.</div>
-  }
-  return <>
-  {items && items.map(()=>{
-    
-  })}
-  </>;
+  useEffect(() => {
+    fetchURL();
+  }, [count]);
+
+  if (loading) {
+    return <div>Loading your data . Please wait.</div>;
+  } else
+    return (
+      <div className="main">
+        <div className="container">
+          {items &&
+            items.map((img) => (
+              <div className="img-container">
+                <img src={img.images[0]} key={img.id} />
+              </div>
+            ))}
+        </div>
+
+        {items.length === 100 ? (
+          "You have viewed all products."
+        ) : (
+          <button onClick={() => setCount(count + 1)}>Load More...</button>
+        )}
+      </div>
+    );
 };
 
 export default Loading;
